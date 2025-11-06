09cc4514ed887e1476677c33b9fe1f3cefcf6143
odintroli19 <sturetroly1@gmail.com>
2025-11-06 02:34:51 +0100

    commentary on the function and classes py file [mapping out the plan]

---
+++ dna_analysis.py
@@ -0,0 +1,6 @@
+### classes : simulationbox, sphere , 
+
+### functions : generate_random_points , create_random_sphere, count_points_in_spheres, monte carlo , and one to calculate pi from sphere, 
+### we need one to read the dna coordinates , and one function to conver pm to angstrom
+
+### possible others functions adn even classes
