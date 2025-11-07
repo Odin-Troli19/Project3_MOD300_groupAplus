135aa091d78023b823a5f6f8bd21f8367463be50
odintroli19 <sturetroly1@gmail.com>
2025-11-07 19:20:11 +0100

    some comments about the classes and functions

--- dna_analysis.py
+++ dna_analysis.py
@@ -4,3 +4,12 @@
 ### we need one to read the dna coordinates , and one function to conver pm to angstrom
 
 ### possible others functions adn even classes
+
+# simulationbox:
+# A helper class for Monte Carlo sims:
+# should define an axis-aligned 3D box (min/max in x,y,z), 
+# ---> sizes and volume
+
+
+# class sphere
+
