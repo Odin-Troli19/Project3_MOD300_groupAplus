d58c4bf5902e06e3e5119cc9c6f0dd278b82888c
odintroli19 <sturetroly1@gmail.com>
2025-11-09 12:37:02 +0100

    functions and classes 01

--- dna_analysis.py
+++ dna_analysis.py
@@ -1,15 +1,5 @@
-### classes : simulationbox, sphere , 
 
-### functions : generate_random_points , create_random_sphere, count_points_in_spheres, monte carlo , and one to calculate pi from sphere, 
-### we need one to read the dna coordinates , and one function to conver pm to angstrom
-
-### possible others functions adn even classes
-
-# simulationbox:
-# A helper class for Monte Carlo sims:
-# should define an axis-aligned 3D box (min/max in x,y,z), 
-# ---> sizes and volume
-
-
-# class sphere
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import Tuple, List, Optional
 
